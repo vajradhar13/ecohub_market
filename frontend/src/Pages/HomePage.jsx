51b2108a20053a4a79fa51d78fa71b7d882d3aae
ChakradharZ99 <115907803+ChakradharZ99@users.noreply.github.com>
2024-01-28 18:47:30 +0530

    v0.1

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { ExampleNavbarFour } from "../Components/NavBarPostLogin"
+import { ProductThree } from "../Components/ProductCards"
+
+function HomePage() {
+  return (
+    <div>
+        <ExampleNavbarFour/>
+        <ProductThree />
+    </div>
+  )
+}
+
+export default HomePage
